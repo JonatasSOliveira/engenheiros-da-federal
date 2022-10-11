aab0f38619c63b0367e5adeda9f0f4b5e82e3c06
Jonatas S. Oliveira <dev.jonatas.oliveira@gmail.com>
2022-10-10 22:44:49 -0300

    Update lista procedures

--- Exercícios Procedure 2022-10-03/Exercícios Procedure - Jonatas S Oliveira.sql
+++ Exercícios Procedure 2022-10-03/Exercícios Procedure - Jonatas S Oliveira.sql
@@ -7,6 +7,87 @@
 -- - Faça a segunda com uma estrutura de decisão; 
 -- - Faça a terceira que gere erro, impedindo a ação;
 -- - Faça a quarta com if e else. 
+
+DROP PROCEDURE IF EXISTS pcd_var_select;
+
+DELIMITER //
+CREATE PROCEDURE pcd_var_select()
+BEGIN
+	DECLARE var INT;
+    SELECT id INTO var FROM produto LIMIT 1;
+END;
+//
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS atualiza_preco
+
+DELIMITER //
+CREATE PROCEDURE atualiza_preco(id_produto INT, novo_preco_custo DOUBLE(12, 2), novo_preco_venda DOUBLE(12, 2))
+BEGIN
+	DECLARE produto_existe BOOL DEFAULT FALSE;
+    SELECT (
+		SELECT COUNT(*) 
+        FROM produto 
+        WHERE id = id_produto
+	) = 1 INTO produto_existe;
+    
+    IF produto_existe THEN
+		UPDATE produto
+        SET preco_custo = novo_preco_custo
+			,preco_venda = novo_preco_venda
+        WHERE id = id_produto;
+    END IF;
+END;    
+//
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS cadastrar_ivenda;
+
+DELIMITER //
+CREATE PROCEDURE cadastrar_ivenda(id_venda INT, id_produto INT, quantidade INT, preco_unidade DOUBLE(12, 2), desconto DECIMAL(12,2))
+BEGIN
+	DECLARE produto_existe BOOL DEFAULT FALSE;
+    SELECT (
+		SELECT COUNT(*) 
+        FROM produto 
+        WHERE id = id_produto
+	) = 1 INTO produto_existe;
+    
+    IF NOT produto_existe THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'PRODUTO NÃO EXISTE';
+    END IF;
+    
+    INSERT INTO ivenda (venda_id, produto_id, quantdade, preco_unidade, desconto)
+    VALUES (id_venda, id_produto, quantidade, preco_unidade, desconto);
+END;
+//
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS atualizar_categoria_produto;
+
+DELIMITER //
+CREATE PROCEDURE atualizar_categoria_produto(id_produto INT, id_categoria INT)
+BEGIN
+	DECLARE produto_categoria_existe INT DEFAULT FALSE;
+    SELECT (
+		SELECT COUNT(*) = 1 
+		FROM produto_categoria 
+		WHERE produto_id = id_produto
+	) INTO produto_categoria_existe;
+        
+	IF produto_categoria_existe THEN
+		UPDATE produto_categoria 
+			SET categoria_id = id_categoria
+			WHERE produto_id = id_produto;
+    ELSE
+		INSERT INTO produto_categoria (produto_id, categoria_id)
+			VALUES(id_produto, id_categoria);
+    END IF;    
+END;
+//
+DELIMITER ;
+
 -- 02 - Escreva uma procedure que registre a baixa de um produto e já atualize devidamente o estoque do produto. Antes das ações, verifique se o produto é ativo.
 -- 03 - Escreva uma procedure que altere o preço de um produto vendido (venda já realizada - necessário verificar a existência da venda). Não permita altearções abusivas - preço de venda abaixo do preço de custo. É possível implementar esta funcionalidade sem a procedure? Se sim, indique como, bem como as vantagens e desvantagens.
 -- 04 - Escreva uma procedure que registre vendas de produtos e já defina o total da venda. É possível implementar a mesma funcionalidade por meio da trigger? Qual seria a diferença?
