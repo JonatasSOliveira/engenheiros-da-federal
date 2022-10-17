90bf3bdc4315c21fd2f05c004df08358ca71da04
Bruno Medeiros <104508954+brunomeedeiros@users.noreply.github.com>
2022-10-17 11:37:28 -0300

    alteração procedure

--- Exercícios Procedure 2022-10-03/Exercícios Procedure - Bruno Medeiros.sql
+++ Exercícios Procedure 2022-10-03/Exercícios Procedure - Bruno Medeiros.sql
@@ -2,11 +2,26 @@
 /******************** PROCEDURES ******************** */
 
 
--- 01 - Escreva quarto procedures de sintaxe - não precisa ter funcionalidade, basta não dar erro de sintaxe. Use variável global para testar.
--- a uma declarando variáveis e com select into; 
--- Faça a segunda com uma estrutura de decisão; 
--- Faça a terceira que gere erro, impedindo a ação;
--- Faça a quarta com if e else. 
+-- 01 - Escreva quarto procedures de sintaxe - não precisa ter funcionalidade, 
+-- basta não dar erro de sintaxe. 
+-- Use variável global para testar. a uma
+
+
+-- #1 -  declarando variáveis e com select into;  
+DELIMITER //
+CREATE PROCEDURE verifica_par_impar (numero INT)
+BEGIN
+    DECLARE ativo CHAR;
+    SELECT cliente.ativo INTO ativo FROM cliente WHERE cliente.id = id_cliente;
+    IF ativo = 'N' THEN
+    BEGIN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'CLIENTE INATIVO';
+    END;
+    END IF;
+    INSERT INTO venda (cliente_id) VALUES (id_cliente);
+END;
+//
+DELIMITER ;
 
 DELIMITER //
 CREATE PROCEDURE insere_venda (id_cliente INT)
@@ -28,6 +43,12 @@ CALL insere_venda(1); -- executando procedure
 
 SELECT * FROM venda; -- verificando se deu certo
 CALL insere_venda(4); -- deverá gerar erro
+
+-- #2 - Faça a segunda com uma estrutura de decisão; 
+
+-- #3 - Faça a terceira que gere erro, impedindo a ação;
+
+-- #4 - Faça a quarta com if e else. 
 
 
 -- 02 - Escreva uma procedure que registre a baixa de um produto e já atualize devidamente o estoque do produto. Antes das ações, verifique se o produto é ativo.
